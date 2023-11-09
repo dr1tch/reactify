b68e2a5a88daab46334d068ce9a193c6caa914c0
Youssouf Kacemi <youssouf.kacemi@evercam.io>
2023-11-10 00:31:40 +0100

    another try

--- scripts/playing-with-git.js
+++ scripts/playing-with-git.js
@@ -15,7 +15,7 @@ const message = execSync('git log -1 --pretty=%B').toString().trim()
 console.log({message})
 
 // get changed files
- const changedFiles =execSync('git --no-pager diff --name-only origin/${{github.head_ref}} origin/${{github.base_ref}}')
+ const changedFiles =execSync('::set-output name=all::$(git diff --name-only --diff-filter=ACMRT ${{ github.event.pull_request.base.sha }} ${{ github.sha }} | xargs)')
 //  const anotherlist = execSync('git --no-pager diff --name-only HEAD^ HEAD')
 
 console.log({ changedFiles, anotherlist })
