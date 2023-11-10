f60286a1d699b797cd9bceb59b30153616693f69
Youssouf Kacemi <youssouf.kacemi@evercam.io>
2023-11-10 02:33:43 +0100

    REMOVE CONSOLES

--- scripts/playing-with-git.js
+++ scripts/playing-with-git.js
@@ -20,34 +20,31 @@ const main = async() => {
         const repo = process.env.repo.split('/')[1];
         const pr_number = prNum;
         const token = process.env.token;
-        console.log({ owner, repo, pr_number, token })
-            /**
-             * Now we need to create an instance of Octokit which will use to call
-             * GitHub's REST API endpoints.
-             * We will pass the token as an argument to the constructor. This token
-             * will be used to authenticate our requests.
-             * You can find all the information about how to use Octokit here:
-             * https://octokit.github.io/rest.js/v18
-             **/
+        /**
+         * Now we need to create an instance of Octokit which will use to call
+         * GitHub's REST API endpoints.
+         * We will pass the token as an argument to the constructor. This token
+         * will be used to authenticate our requests.
+         * You can find all the information about how to use Octokit here:
+         * https://octokit.github.io/rest.js/v18
+         **/
         const octokit = new github.getOctokit(token);
-        console.log({ octokit })
-            /**
-             * We need to fetch the list of files that were changes in the Pull Request
-             * and store them in a variable.
-             * We use octokit.paginate() to automatically loop over all the pages of the
-             * results.
-             * Reference: https://octokit.github.io/rest.js/v18#pulls-list-files
-             */
+        /**
+         * We need to fetch the list of files that were changes in the Pull Request
+         * and store them in a variable.
+         * We use octokit.paginate() to automatically loop over all the pages of the
+         * results.
+         * Reference: https://octokit.github.io/rest.js/v18#pulls-list-files
+         */
         const { data: changedFiles } = await octokit.rest.pulls.listFiles({
             owner,
             repo,
             pull_number: pr_number,
         });
-        console.log({ changedFiles })
-            /**
-             * Contains the sum of all the additions, deletions, and changes
-             * in all the files in the Pull Request.
-             **/
+        /**
+         * Contains the sum of all the additions, deletions, and changes
+         * in all the files in the Pull Request.
+         **/
         let diffData = {
             additions: 0,
             deletions: 0,
@@ -125,17 +122,14 @@ const main = async() => {
         // get branch name
         const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
 
-        console.log({ branch })
 
         // get commit hash
         const commit = execSync('git rev-parse HEAD').toString().trim()
 
-        console.log({ commit })
 
         // get commit message
         const message = execSync('git log -1 --pretty=%B').toString().trim()
 
-        console.log({ message })
 
 
 
@@ -143,30 +137,21 @@ const main = async() => {
         const uiRegex = /packages\/ui\/.*\.*/
         const changed = changedFiles.map(file => file.filename).filter(file => uiRegex.test(file)).length > 0
         console.log({ changed })
-
-        // verify if a file inside ui has changed with string check
+            // verify if a file inside ui has changed with string check
         const changed2 = changedFiles.map(file => file.filename).filter(file => file.includes('packages/ui')).length > 0
-        console.log({ changed2 })
 
         // console.log({process: process.env})
         // get the pull request name inside 
         const pullRequestName = execSync('git log -1 --pretty=%s').toString().trim()
-        console.log({ pullRequestName })
 
         // get the pull request number
         const pullRequestNumber = execSync('git log -1 --pretty=%s').toString().trim()
-        console.log({ pullRequestNumber })
 
         // get the pull request author
         const pullRequestAuthor = execSync('git log -1 --pretty=%an').toString().trim()
         const pullRequestTriggeringActor = process.env.GITHUB_TRIGGERING_ACTOR || null
-        console.log({ pullRequestAuthor, pullRequestTriggeringActor })
 
-        const prTitle = process.env.GITHUB_EVENT_NAME === 'pull_request' ? process.env.GITHUB_PULL_REQUEST_TITLE : null;
-        const prNumber = process.env.GITHUB_EVENT_NAME === 'pull_request' ? process.env.GITHUB_PULL_REQUEST_NUMBER : null;
 
-        console.log('Pull Request Title:', prTitle);
-        console.log('Pull Request Number:', prNumber);
 
     } catch (error) {
         console.log({ error })
