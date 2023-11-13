43a8ceb2cf68d1c590fb3be566409559b3686eef
Halim <halim.bouguedra@gmail.com>
2023-11-13 15:18:18 +0000

    Debug ci-merge

--- scripts/ci-merge.js
+++ scripts/ci-merge.js
@@ -14,11 +14,14 @@ const mainMerge = async() => {
         console.log({ EventPath: JSON.stringify(eventPath, null, 2) });
         const newVersion = getNewVersion();
 
-        console.log('Upgrading version to: ', newVersion);
+        const bumpVersionCommand = [
+            'cd packages/ui',
+            `git config user.email "github-actions@github.com"`,
+            `yarn version --new-version ${newVersion}`,
+            `git push`
+        ].join("&&")
 
-
-        const gitResult = execSync('cd packages/ui && git config user.email "github-actions@github.com" && git config user.name "github-actions[bot]" && ' +
-            `newVersion=$(yarn version --new-version ${newVersion} | grep -oP "(?<=to\s).*(?=:)") && ` + "git log --oneline && " + "git rev-parse --abbrev-ref HEAD", { encoding: 'utf-8' });
+        execSync(bumpVersionCommand, {encoding: "utf-8"})
 
         console.log('Building the package...');
         buildPackage();
@@ -28,8 +31,13 @@ const mainMerge = async() => {
 
         console.log('Merge processing completed.');
     } catch (error) {
-        console.log({ error: JSON.stringify(error.toString('utf-8'), null, 2) })
-
+        console.log({ error });
+        console.log("--------------------")
+        console.log("--------------------")
+        console.log(error.stderr.toString())
+        console.log("--------------------")
+        console.log("--------------------")
+        core.setFailed(error.message);
     }
 };
 
