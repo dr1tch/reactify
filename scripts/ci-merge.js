b76d285a941dc0f39dd08f1f2d492c32a4217be4
Youssouf Kacemi <youssouf.kacemi@evercam.io>
2023-11-13 14:40:29 +0100

    FIX GIT IDENTITY ISSUE

--- scripts/ci-merge.js
+++ scripts/ci-merge.js
@@ -12,7 +12,7 @@ const mainMerge = async() => {
     }
     try {
         console.log({ EventPath: JSON.stringify(eventPath, null, 2) });
-
+        execSync('cd packages/ui && git config user.email "github-actions@github.com" && git config user.name "github-actions[bot]"');
         const newVersion = getNewVersion();
 
         console.log('Upgrading version to: ', newVersion);
