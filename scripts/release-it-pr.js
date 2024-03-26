a77ddca7d8e2ebbed5b3affce96b0fa35e22b79a
Youssouf Kacemi <youssouf.kacemi@gmail.com>
2024-03-26 19:03:15 +0100

    trigger build

--- scripts/release-it-pr.js
+++ scripts/release-it-pr.js
@@ -37,26 +37,8 @@ async function main() {
     } else {
         data.version = `${getNewVersion(version)}`
     }
-
-    console.log({
-        isPreview,
-        commit,
-        date,
-        branchName,
-        commitsListFromMaster,
-        pkgVersion,
-        pkg: JSON.stringify(data, null, 2),
-        version: data.version,
-        data
-    })
-
-    const pwd = process.cwd()
-    console.log({ pwd })
-
     await fsPromises
         .writeFile(pkgFile, JSON.stringify(data, null, 2), "utf-8")
-
-
     console.log("Building the package...")
     const buildOutput = execSync(`cd packages/ui && yarn build`, { encoding: 'utf-8' });
     console.log('Build Output: \n', buildOutput);
@@ -75,16 +57,19 @@ async function main() {
         })
     )
     RootData.dependencies[data.name] = data.version
-    console.log({ rootPKGFile, RootData })
     await fsPromises
         .writeFile(rootPKGFile, JSON.stringify(RootData, null, 2), "utf-8")
         // create a .releases folder, with a file named after the version and with a changelog inside it
     const releaseFolder = resolve('.releases')
     await fsPromises.mkdir(releaseFolder, { recursive: true })
+
     const releaseFile = resolve(releaseFolder, `${data.version}.md`)
     const changelog = commitsListFromMaster.map((commit) => `- ${commit}`).join('\n')
     await fsPromises.writeFile(releaseFile, changelog, "utf-8")
     console.log({ releaseFile, changelog })
+    console.log('changed files:')
+    const changedFiles = execSync(`git status --porcelain`, { encoding: 'utf-8' });
+    console.log({ changedFiles })
     console.log("Committing changes...")
     const releaseAdd = execSync(`git add .releases package.json packages/ui/`, { encoding: 'utf-8' });
     const releaseCommit = execSync(`git commit -m "chore: release ${data.name}@${data.version}"`, { encoding: 'utf-8' });
