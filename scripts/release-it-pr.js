f2082da2563bd3364859df6df84aa4668f981447
Youssouf Kacemi <youssouf.kacemi@gmail.com>
2024-03-27 03:28:25 +0100

    install just action package

--- scripts/release-it-pr.js
+++ scripts/release-it-pr.js
@@ -3,7 +3,6 @@ import { resolve, join } from "path"
 import { execSync } from "child_process"
 import os from "os"
 import * as github from '@actions/github';
-import * as core from '@actions/core';
 const token = process.env.GITHUB_TOKEN
 const octokit = new github.getOctokit(token);
 
