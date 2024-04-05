20b32f61443065ff645e7ad85f282a7fe7ab34c6
Youssouf Kacemi <youssouf.kacemi@gmail.com>
2024-04-05 02:41:28 +0100

    again

--- deploy/test.js
+++ deploy/test.js
@@ -2,6 +2,14 @@ const fs = require("fs")
 const { resolve, join } = require("path")
 const execSync = require("child_process").execSync
 const os = require("os")
+const moment = require("moment")
+const latestCommitHash = execSync("git rev-parse HEAD").toString().trim()
+
+function rollback() {
+    console.log("Rolling back changes...")
+    execSync(`git reset --hard ${latestCommitHash}`)
+    console.log("Changes rolled back successfully.")
+}
 
 function getCurrentBranchName() {
     return execSync("git rev-parse --abbrev-ref HEAD").toString().trim()
@@ -60,9 +68,9 @@ function checkPackageAvailability(packageName, version) {
 function installPkgs(name, version) {
     try {
         if (checkPackageAvailability(name, version)) {
-            execSync("yarn install", {
-                encoding: "utf-8",
-            })
+            // Install the package
+            console.log(`Installing ${name}@${version}`)
+            execSync("yarn", { encoding: "utf-8" })
         } else {
             installPkgs(name, version)
         }
@@ -70,6 +78,57 @@ function installPkgs(name, version) {
         console.error({ error })
     }
 }
+
+function updateChangelog(version) {
+    try {
+        const changelogPath = resolve("packages/ui", "CHANGELOG.md")
+        console.log("Reading changelog...")
+        let changelogContent = ""
+        try {
+            changelogContent = fs.readFileSync(changelogPath, "utf-8")
+            console.log("Changelog read successfully.")
+        } catch {
+            console.log(
+                'Changelog file doesn\'t exist. Creating "CHANGELOG.md" file...'
+            )
+            changelogContent = ""
+            fs.appendFileSync(changelogPath, "")
+            console.log("Changelog file created!")
+        }
+        const branchName = getCurrentBranchName()
+            // get new updates and format: "* <Commit Message> (<Issue Number>) <Commit Hash>"
+        const newUpdates = execSync(
+            `git log --no-merges --pretty=format:"* %s ([%h](https://github.com/dr1tch/reactify/commit/%H))" master..${branchName}`, { encoding: "utf-8" }
+        )
+        if (newUpdates === "") {
+            console.log("No new updates found.")
+
+            return
+        }
+        const releaseHeader = `## ${version} (${moment().format("YYYY-MM-DD")})\n\n`
+        const header = "# Changelog \n\n"
+
+        let newChangelogContent
+
+        if (!changelogContent.startsWith(header)) {
+            newChangelogContent =
+                header + releaseHeader + newUpdates + "\n\n" + changelogContent
+        } else {
+            newChangelogContent =
+                header +
+                releaseHeader +
+                newUpdates +
+                "\n\n" +
+                changelogContent.substring(header.length)
+        }
+
+        fs.writeFileSync(changelogPath, newChangelogContent)
+        console.log("Changelog updated successfully.")
+    } catch (error) {
+        console.error("Error updating changelog:", error)
+    }
+}
+
 async function main() {
     let branchName = ""
     if (process.env.GITHUB_HEAD_REF) {
@@ -78,6 +137,7 @@ async function main() {
         branchName = getCurrentBranchName()
     }
     const changedFiles = listChangedFiles()
+    console.log({ changedFiles })
     const isPackageChanged = changedFiles.some((file) =>
             file.startsWith("packages/ui")
         )
@@ -87,22 +147,21 @@ async function main() {
 
         return process.exit(0)
     }
+    console.log("Bumping version...")
     const pkgFile = resolve("packages/ui", "package.json")
     let packageJson = JSON.parse(await fs.promises.readFile(pkgFile, "utf-8"))
     const commitHash = execSync("git rev-parse --short HEAD")
         .toString("utf-8")
         .trim()
-        // let packageJson = require('./package.json'); // Adjust the path as necessary
     const pkgVersion = branchName.split("/").join("-")
     const version = packageJson.version.split("-")[0]
     const previewVersion = `${version}-${pkgVersion}-${commitHash}`
     if (branchName !== "main" && branchName !== "master") {
-        //   const safeBranchName = makeBranchSafeForNpm(branchName)
-        // previewVersion.version = `${baseVersion}-${safeBranchName}`;
         packageJson.version = previewVersion
     } else {
         packageJson.version = getNewVersion(version)
     }
+    console.log(`New version: ${packageJson.version}`)
     console.log("Generating new .npmrc file...")
     const npmrcPath = join(os.homedir(), ".npmrc")
     const nodeAuthToken = process.env.NODE_AUTH_TOKEN
@@ -117,24 +176,30 @@ async function main() {
         }
     }
     await fs.promises.writeFile(
-            pkgFile,
-            JSON.stringify(packageJson, null, 2),
-            "utf-8"
-        ) // Adjust the path as necessary
-    const changesAdded = execSync("git status").toString().trim()
-    if (changesAdded) {
-        execSync(
-            `git commit -am "updating package version to ${packageJson.version}"`
-        )
+        pkgFile,
+        JSON.stringify(packageJson, null, 2),
+        "utf-8"
+    )
+
+    console.log("Build and Publish package...")
+    execSync(
+        `cd packages/ui && yarn install && yarn build && yarn publish --non-interactive --new-version ${packageJson.version} --access public`, {
+            encoding: "utf-8",
+        }
+    )
+
+    console.log("Updating changelog...")
+    const isPreview = branchName !== "master"
+    if (!isPreview) {
+        updateChangelog(packageJson.version)
     }
-    execSync("cd packages/ui && yarn release-it --verbose", { encoding: "utf-8" })
-    const rootPackagePath = resolve("package.json") // Adjust the path to your root package
+    console.log("Updating root package.json...")
+    const rootPackagePath = resolve("package.json")
 
     const rootPackageJson = JSON.parse(
         await fs.promises.readFile(rootPackagePath, "utf-8")
     )
 
-    // Assuming your package is a dependency in the root package.json
     rootPackageJson.dependencies[packageJson.name] = packageJson.version
 
     await fs.promises.writeFile(
@@ -143,13 +208,12 @@ async function main() {
     )
     console.log("Installing packages again..")
     installPkgs(packageJson.name, packageJson.version)
-    execSync(`git branch --set-upstream-to=origin/${branchName ?? "HEAD^"}`)
     const pwd = execSync("pwd").toString().trim()
     console.log(`Writing package.json in ${pwd}`)
     console.log("Committing and pushing changes...")
     const rootCommitCommands = [
-        `git commit -am "updating ${packageJson.name} to ${packageJson.version}"`,
-        "git push",
+        `git commit -am "chore: ${packageJson.name} release: v-${packageJson.version}"`,
+        `git push origin ${branchName ?? "HEAD^"}`,
     ].join(" && ")
     execSync(rootCommitCommands, {
         encoding: "utf-8",
@@ -158,6 +222,7 @@ async function main() {
 }
 
 main().catch((error) => {
-    console.error(error)
+    console.error({ error })
+    rollback()
     process.exit(1)
 })
